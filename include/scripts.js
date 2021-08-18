f8fbc6aa147a7e8b9e9383d1cc6a78679281f88f
xand <acatley04@gmail.com>
2021-08-18 12:24:15 +0100

    Update

--- include/scripts.js
+++ include/scripts.js
@@ -6,6 +6,10 @@ var Contents   = document.getElementById("Contents");
 var Toggle     = document.getElementById("HideContents");  
 var Chapters   = Headers.children;
 
+var title = document.location.href.split('/');
+title = title[title.length - 2];
+document.title = title.replace('.github.io','');
+
 function getGitURL() {
     document.location = 'https://github.com/xandprojects/xandprojects.github.io/tree/main/' + document.location.href.split('.io/')[1].replace('index','').replace('.html','');
 }
